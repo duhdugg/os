2867921ea0eed360ebdf5017bb9e7d6da99781eb
Doug Elkin <1406429+duhdugg@users.noreply.github.com>
2026-03-02 10:10:15 -0500

    light refactor

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
-mapfile -t ADD_PACKAGES < <(grep -v '^#' /ctx/packages/dnf-add)
+mapfile -t ADD_GROUP_PACKAGES < <(grep -v '^#' /ctx/packages/dnf-group-install)
+mapfile -t ADD_PACKAGES < <(grep -v '^#' /ctx/packages/dnf-install)
 
+dnf group install -y "${ADD_GROUP_PACKAGES[@]}"
 dnf install -y "${ADD_PACKAGES[@]}"
